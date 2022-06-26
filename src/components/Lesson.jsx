8d7270d6351ba23b445224a265516fea7e5a567d
Thobias Stahlschmidt <thobias_stahlschmidt@whirlpool.com>
2022-06-26 19:57:52 -0300

    feat: add condition into tailwindcss

--- src/components/Lesson.jsx
+++ src/components/Lesson.jsx
@@ -1,14 +1,18 @@
 import { CheckCircle, Lock } from 'phosphor-react'
 import { format, isPast } from "date-fns"
 import ptBR from 'date-fns/esm/locale/pt-BR/index.js';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 export function Lesson(props) {
+  const { slug } = useParams();
+
   const date = new Date(props.availableAt);
   const isAvailable = isPast(date);
   const availableDateFromatted = format(date, "EEEE' • 'd' de 'MMMM' • 'k'h'mm", {
     locale: ptBR
   })
+
+  const isActiveLesson = (slug === props.slug);
 
   return (
     <Link to={`/event/lesson/${props.slug}`} className='group'>
@@ -16,7 +20,7 @@ export function Lesson(props) {
         {availableDateFromatted}
       </span>
 
-      <div className="rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500">
+      <div className={`rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500 ${isActiveLesson ? 'bg-green-500' : ''}`}>
         <header className="flex items-center justify-between">
           {isAvailable ? (
             <span className="text-sm text-blue-500 font-medium flex items-center gap-2">
